ce24b77ce3b157e23df4c737762e71a54c2e3704
202407034 <vcn1234@daum.net>
2025-06-05 20:03:27 +0900

    [ - ] 빼기

---
+++ sub.cpp
@@ -0,0 +1,15 @@
+#include <add.h>
+#include <sub.h>
+#include <stdio.h>
+
+int sub(int x, int y)
+{
+	int result = x;
+
+	for(int i = 0; i < y; i++)
+		{
+			result = add(result -1);
+		}
+	printf("빼기: %d\n", result);
+	return result
+}
